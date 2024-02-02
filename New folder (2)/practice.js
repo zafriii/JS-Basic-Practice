e1748459cd1cd54aca5379421cf8e86b3179c3e1
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-02-02 15:12:47 +0600

    Create practice.js

---
+++ New folder (2)/practice.js
@@ -0,0 +1,14 @@
+//Factorial
+const factorial = (n) => {
+  
+    if(n==0 || n==1){
+        return  1;
+    }
+    else{
+        return   n * factorial(n-1);
+    }
+}
+
+let ans = factorial(5)
+
+console.log(ans)
